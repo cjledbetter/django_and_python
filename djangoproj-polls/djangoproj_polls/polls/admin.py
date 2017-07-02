30816bd6c78059f15d22554c9d761e33b28b7772
Chris <cjledbetter@gmail.com>
2017-07-02 18:15:39 -0400

    DjangoProject Tutorial

---
+++ djangoproj-polls/djangoproj_polls/polls/admin.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+from django.contrib import admin
+
+from .models import Question
+
+#from __future__ import unicode_literals
+
+#from django.contrib import admin
+
+# Register your models here.
+
+admin.site.register(Question)
