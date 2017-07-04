03cea4ba6f4be22357b853f5857bc3d52d8b2655
Chris <cjledbetter@gmail.com>
2017-07-04 15:27:40 -0400

    DjangoBook chap 3

--- djangobook/djangobook/djangobook/views.py
+++ djangobook/djangobook/djangobook/views.py
@@ -1,8 +1,24 @@
 from django.http import HttpResponse
 
+import datetime
+
 def homepage(request):
     return HttpResponse("Welcome to the Home Page View!")
 
 def hello(request):
     return HttpResponse("Hello World!")
 
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now now %s.</body></html>" % now
+    return HttpResponse(html)
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404()
+    dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be  %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
+                        
